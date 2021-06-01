f3b5922e63f02254ae6c20288bee4c9a16325863
dong-pro <wydong_39@126.com>
2021-06-01 10:41:50 +0800

    basic部分细节修改

--- basic/day08_15filefunction/day15/04_函数.py
+++ basic/day08_15filefunction/day15/04_函数.py
@@ -5,7 +5,7 @@ def func2():
     print('不好')
 
 def gn(fn):  # fn是一个参数. 根据实参给的值的变化而变化
-    print(fn.__name__)  # func
+    print(fn.__name__)  # func2
     fn()
 
 gn(func2)
--- basic/day27_31netpro/day31/03_计算文件大小.py
+++ basic/day27_31netpro/day31/03_计算文件大小.py
@@ -1,4 +1,4 @@
 import os
 
-size = os.stat(r'/Users/wangyadong/py_train/py_old_boy_15/pytrain_05_netpro/day31/01_今日内容大纲').st_size
+size = os.stat(r'/Users/wangyadong/py_train/py_old_boy_15/pytrain_05_netpro/day31/00_今日内容大纲').st_size
 print(size)
