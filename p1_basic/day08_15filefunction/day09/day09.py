488971fa45e946b525c477acc499a0e11addfde7
dong-pro <55371249+dong-pro@users.noreply.github.com>
2025-12-16 13:58:04 +0800

    Update day09.py
    优化时间复杂度

--- p1_basic/day08_15filefunction/day09/day09.py
+++ p1_basic/day08_15filefunction/day09/day09.py
@@ -26,3 +26,16 @@ for ren in list3:  # {'name': 'admin', 'hobby': '抽烟'},
         dic['hobby_list'] = [ren['hobby']]
         list4.append(dic)  # 第一个人进去
 print(list4)
+
+# 时间复杂度从 O (n²) 降到 O (n)，数据量大时效率更高
+temp = {}  # 临时字典：key=姓名，value=爱好列表
+for item in list3:
+    name = item['name']
+    hobby = item['hobby']
+    if name not in temp:
+        temp[name] = []  # 初始化列表
+    temp[name].append(hobby)
+
+# 转换为目标格式
+list4 = [{'name': k, 'hobby_list': v} for k, v in temp.items()]
+print(list4)
